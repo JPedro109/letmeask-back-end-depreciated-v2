9d15af35215849fc54e0e74bc314b7381d2ce0fd
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-02-02 21:15:04 -0300

    test: add room entitie unit test

---
+++ tests/layers/entities/room/room-code.spec.ts
@@ -0,0 +1,29 @@
+import { RoomCode, InvalidRoomCodeError } from "@/layers/entities";
+
+describe("Object Value - RoomCode", () => {
+
+	test("Should not create room code, because room code is empty" , () => {
+		const invalidRoomCode = "";
+
+		const sut = RoomCode.create(invalidRoomCode);
+
+		expect(sut).toBeInstanceOf(InvalidRoomCodeError);
+	});
+
+	test("Should not create room code, because the room code is not 6 characters long" , () => {
+		const invalidRoomCode = "c";
+
+		const sut = RoomCode.create(invalidRoomCode);
+
+		expect(sut).toBeInstanceOf(InvalidRoomCodeError);
+	});
+
+	test("Should create room code" , () => {
+		const roomCode = "123456";
+
+		const sut = RoomCode.create(roomCode);
+
+		if(!(sut instanceof Error)) expect(sut.value).toBe(roomCode);
+		expect(sut).toBeInstanceOf(RoomCode);
+	});
+});
