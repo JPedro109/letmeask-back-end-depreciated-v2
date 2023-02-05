8e5a9c9754f4fa68c6c60db87de90da6d6a8c3c5
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-02-04 22:34:31 -0300

    refactor: export ports in a singles files

--- src/layers/use-cases/ports/index.ts
+++ src/layers/use-cases/ports/index.ts
@@ -1,1 +1,13 @@
 export * from "./cache/protocol";
+
+export * from "./cryptography/protocol";
+
+export * from "./generation/protocol";
+
+export * from "./json-web-token/protocol";
+export * from "./json-web-token/type";
+export * from "./json-web-token/error";
+
+export * from "./repositories";
+
+export * from "./mail/protocol";
