9d85141736c280f027b5ddef1fd8d940bfbd5b56
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-02-06 13:27:34 -0300

    feat: add use case of get username

--- src/layers/use-cases/use-cases/user/index.ts
+++ src/layers/use-cases/use-cases/user/index.ts
@@ -5,3 +5,7 @@ export * from "./create-user/dtos";
 export * from "./delete-user/use-case";
 export * from "./delete-user/protocol";
 export * from "./delete-user/dtos";
+
+export * from "./get-username/use-case";
+export * from "./get-username/protocol";
+export * from "./get-username/dtos"; 
