89c65a4df546ece6d04d80b6ea95178ca7d9fdde
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-02-09 13:43:23 -0300

    test: add update username use case unit test

---
+++ tests/layers/use-cases/user/update-username.spec.ts
@@ -0,0 +1,48 @@
+import { UserRepositoryStub } from "../__mocks__";
+
+import { InvalidUsernameError } from "@/layers/entities";
+import { NotFoundError, UpdateUsernameUseCase } from "@/layers/use-cases";
+
+const makeSut = () => {
+	const userRepositoryStub = new UserRepositoryStub();
+	const sut = new UpdateUsernameUseCase(userRepositoryStub);
+
+	return {
+		userRepositoryStub,
+		sut
+	};
+};
+
+describe("Use case - UpdateUsernameUseCase", () => {
+    
+	test("Should not update username, because username is invalid", async () => {
+		const id = "1";
+		const username = "u".repeat(300);
+		const { sut } = makeSut();
+
+		const result = await sut.execute({ id, username });
+
+		expect(result).toBeInstanceOf(InvalidUsernameError);
+	});
+
+	test("Should not update username, because username is not exists", async () => {
+		const id = "2";
+		const username = "username";
+		const { sut, userRepositoryStub } = makeSut();
+		jest.spyOn(userRepositoryStub, "getUserById").mockResolvedValueOnce(Promise.resolve(null));
+
+		const result = await sut.execute({ id, username });
+
+		expect(result).toBeInstanceOf(NotFoundError);
+	});
+
+	test("Should update username", async () => {
+		const id = "1";
+		const username = "username";
+		const { sut } = makeSut();
+
+		const result = await sut.execute({ id, username });
+
+		expect(result).toBe(username);
+	});
+});
