6acdffdcb50cd52c18db24eb55ad56ded0cbab44
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-02-12 19:45:49 -0300

    feat: add use case of get room

--- src/layers/use-cases/use-cases/room/index.ts
+++ src/layers/use-cases/use-cases/room/index.ts
@@ -5,3 +5,7 @@ export * from "./create-room/protocol";
 export * from "./delete-room/use-case";
 export * from "./delete-room/dtos";
 export * from "./delete-room/protocol";
+
+export * from "./get-room/use-case";
+export * from "./get-room/dtos";
+export * from "./get-room/protocol";
