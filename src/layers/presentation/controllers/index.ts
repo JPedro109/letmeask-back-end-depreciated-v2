9d379de53aec6bfa25570e5427ad1216ba76081c
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-02-18 20:57:17 -0300

    feat: add recover user password controller

--- src/layers/presentation/controllers/index.ts
+++ src/layers/presentation/controllers/index.ts
@@ -1,5 +1,6 @@
 export * from "./user/create-user-controller";
 export * from "./user/delete-user-controller";
 export * from "./user/get-username-controller";
+export * from "./user/recover-user-password-controller";
 
 export * from "./utils";
