b7ed13e824fda7ffb685c25495d526dfee665c30
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-03-02 00:27:38 -0300

    feat: add email provider environment variables

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -2,3 +2,7 @@ import "dotenv/config";
 
 export const APP_URL = process.env.APP_URL;
 export const JWT_KEY = process.env.JWT_KEY;
+export const EMAIL_PROVIDER_EMAIL = process.env.EMAIL_PROVIDER_EMAIL;
+export const PASSWORD_PROVIDER_EMAIL = process.env.PASSWORD_PROVIDER_EMAIL;
+export const HOST_PROVIDER_EMAIL = process.env.HOST_PROVIDER_EMAIL;
+export const PORT_PROVIDER_EMAIL = parseInt(process.env.PORT_PROVIDER_EMAIL);
