4f033409df5548418e076f57a77623c3e95c0b73
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-03-03 00:53:48 -0300

    feat: add databases environment variables

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -6,3 +6,5 @@ export const EMAIL_PROVIDER_EMAIL = process.env.EMAIL_PROVIDER_EMAIL;
 export const PASSWORD_PROVIDER_EMAIL = process.env.PASSWORD_PROVIDER_EMAIL;
 export const HOST_PROVIDER_EMAIL = process.env.HOST_PROVIDER_EMAIL;
 export const PORT_PROVIDER_EMAIL = parseInt(process.env.PORT_PROVIDER_EMAIL);
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const DATABASE_NOSQL_URL = process.env.DATABASE_NOSQL_URL;
