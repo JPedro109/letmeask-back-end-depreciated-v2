878ca7d02f312f73e1bc7b1105819cd98f23abc2
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-03-04 01:12:16 -0300

    feat: add auth middleware

--- src/layers/presentation/index.ts
+++ src/layers/presentation/index.ts
@@ -3,4 +3,5 @@ export * from "./ports";
 export * from "./helpers";
 export * from "./protocols";
 export * from "./controllers";
+export * from "./middlewares";
 export * from "./decorators";
---
+++ src/layers/presentation/middlewares/auth/authenticate-user-middleware.ts
@@ -0,0 +1,23 @@
+import { JsonWebTokenProtocol, UnauthorizedError } from "@/layers/use-cases";
+import { HttpProtocol, HttpRequest, HttpResponse, ok, unauthorized } from "@/layers/presentation";
+
+export class AuthenticateUserMiddleware implements HttpProtocol {
+
+	constructor(private readonly jsonWebToken: JsonWebTokenProtocol) { }
+
+	async handle(request: HttpRequest): Promise<HttpResponse> {
+		const { authorization } = request.headers;
+
+		if (!authorization) return unauthorized(new UnauthorizedError("Você não está logado"));
+    
+		const [bearer, token] = authorization.split(" ");
+
+		if(bearer !== "Bearer") return unauthorized(new UnauthorizedError("Código inválido"));
+    
+		const decode = this.jsonWebToken.verifyToken(token);
+
+		if(decode instanceof Error) return unauthorized(decode);
+
+		return ok(decode.id as string);
+	}
+}
