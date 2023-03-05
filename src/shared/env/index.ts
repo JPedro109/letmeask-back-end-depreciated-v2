8480fad463b10c9e1dc10cb9fbc3f42c67c75b84
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-03-05 19:35:04 -0300

    feat: add port and environment environment variables

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -8,3 +8,5 @@ export const HOST_PROVIDER_EMAIL = process.env.HOST_PROVIDER_EMAIL;
 export const PORT_PROVIDER_EMAIL = parseInt(process.env.PORT_PROVIDER_EMAIL);
 export const DATABASE_URL = process.env.DATABASE_URL;
 export const DATABASE_NOSQL_URL = process.env.DATABASE_NOSQL_URL;
+export const ENVIRONMENT = process.env.ENVIRONMENT;
+export const PORT = parseInt(process.env.PORT);
