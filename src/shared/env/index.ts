955f6027c3da0d3c4003fa721d11bae7706ac7d4
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-04-21 22:39:19 -0300

    refactor: add new environment variables

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -10,3 +10,5 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 export const DATABASE_NOSQL_URL = process.env.DATABASE_NOSQL_URL;
 export const ENVIRONMENT = process.env.ENVIRONMENT;
 export const PORT = parseInt(process.env.PORT);
+export const QUEUE_HOST = process.env.QUEUE_HOST;
+export const QUEUE_NAME = process.env.QUEUE_NAME;
