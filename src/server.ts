5a88aae240ccdf9e35620e353b5e93775b6ebfd7
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-07-18 19:10:56 -0300

    refactor: rename environment variable PORT to PORT_REST

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { PORT } from "@/shared";
+import { PORT_REST } from "@/shared";
 import { DatabaseSQLHelper, DatabaseNoSQLHelper, QueueHelper } from "@/layers/external";
 import { setupRest } from "@/main/rest";
 
@@ -6,6 +6,6 @@ DatabaseSQLHelper.connect()
 	.then(async () => {
 		await DatabaseNoSQLHelper.connect();
 		await QueueHelper.connect();
-		setupRest().listen(PORT || 3333, () => console.log(`Server running at Port ${PORT || 3333}`));
+		setupRest().listen(PORT_REST || 3333, () => console.log(`Server running at Port ${PORT_REST || 3333}`));
 	})
 	.catch(console.error);
