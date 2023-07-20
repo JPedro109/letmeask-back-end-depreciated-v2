9bdecffb60040839181fcd4fe0fd6eebf57cd26b
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-07-20 20:50:12 -0300

    refactor: add environment variable PORT_GRAPHQL

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -6,5 +6,6 @@ export const DATABASE_URL = process.env.DATABASE_URL;
 export const DATABASE_NOSQL_URL = process.env.DATABASE_NOSQL_URL;
 export const ENVIRONMENT = process.env.ENVIRONMENT;
 export const PORT_REST = parseInt(process.env.PORT_REST);
+export const PORT_GRAPHQL = parseInt(process.env.PORT_GRAPHQL);
 export const QUEUE_HOST = process.env.QUEUE_HOST;
 export const QUEUE_NAME = process.env.QUEUE_NAME;
