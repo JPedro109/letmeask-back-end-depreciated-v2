79e5bb3191f83bd7169971f0f6b82e812b5fb5af
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-09-11 23:01:12 -0300

    refactor: annular connection when executing disconnection method

--- src/layers/external/queue/helper/index.ts
+++ src/layers/external/queue/helper/index.ts
@@ -14,6 +14,7 @@ export class QueueHelper {
 	}
 
 	static async disconnect(): Promise<void> {
-		await QueueHelper.connection.close();
+		await QueueHelper.connection?.close();
+		QueueHelper.connection = null;
 	}
 }
