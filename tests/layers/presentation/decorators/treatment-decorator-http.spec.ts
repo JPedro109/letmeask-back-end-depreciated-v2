21922d529846d48f8983f3908ae1fe55ea16d8bd
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-10-27 23:22:12 -0300

    refactor: add unit test of treatment decorator

--- tests/layers/presentation/decorators/treatment-decorator-http.spec.ts
+++ tests/layers/presentation/decorators/treatment-decorator-http.spec.ts
@@ -12,4 +12,14 @@ describe("Presentation - TreatmentDecoratorHttp", () => {
 
 		expect(result).toEqual(serverError());
 	});
+
+	test("Should return server error with logged user", async () => {
+		const controllerStub = new ControllerStub();
+		const logFacadeStub = new LogFacadeStub();
+		const sut = new TreatmentDecoratorHttp(controllerStub, logFacadeStub);
+
+		const result = await sut.http({ userId: "1" });
+
+		expect(result).toEqual(serverError());
+	});
 });
