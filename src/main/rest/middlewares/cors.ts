08930dd2df8af07b071aa406ede5a55d4109757b
João Pedro <joaopedromoraesnsilva@gmail.com>
2023-11-01 20:20:55 -0300

    refactor: modify cors options

--- src/main/rest/middlewares/cors.ts
+++ src/main/rest/middlewares/cors.ts
@@ -3,18 +3,21 @@ import { secretsAdapter } from "@/main/factories";
 import lib from "cors";
 
 const allowList = [ secretsAdapter.getRequiredSecret(SecretsEnum.AppUrl) ];
+const allowHosts = [ secretsAdapter.getSecret(SecretsEnum.ApiHost) ];
+const allowUserAgents = [ secretsAdapter.getSecret(SecretsEnum.PrometheusUserAgent) ];
 
-const corsOptions = {
+const corsOptions = (req, callback) => {
+	const origin = req.header("Origin");
+	const host = req.header("Host");
+	const userAgent = req.header("User-Agent");
+	
+	if(secretsAdapter.getSecret(SecretsEnum.Environment) === "TEST") return callback(null, true);
 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	origin: (origin: string, callback: any) => {
+	if (allowList.indexOf(origin) !== -1) return callback(null, true);
 
-		if(secretsAdapter.getSecret(SecretsEnum.Environment) === "TEST") return callback(null, true);
+	if (allowHosts.indexOf(host) !== -1 && allowUserAgents.indexOf(userAgent) !== -1) return callback(null, true);
 
-		if (allowList.indexOf(origin) !== -1) return callback(null, true);
-
-		callback(new Error("Not allowed cors"));
-	}
+	callback(new Error("Not allowed cors"));
 };
 
 export const cors = lib(corsOptions);
