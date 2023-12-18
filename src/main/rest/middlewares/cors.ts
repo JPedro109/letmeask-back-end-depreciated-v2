8bfbfb6b1bb5a44fdf0babf6ad78bf64f1f60f76
João Pedro <112661258+JPedro109@users.noreply.github.com>
2023-12-18 20:28:36 -0300

    refactor: modify rest cors

--- src/main/rest/middlewares/cors.ts
+++ src/main/rest/middlewares/cors.ts
@@ -3,19 +3,13 @@ import { secretsAdapter } from "@/main/factories";
 import lib from "cors";
 
 const allowList = [ secretsAdapter.getRequiredSecret(SecretsEnum.AppUrl) ];
-const allowHosts = [ secretsAdapter.getSecret(SecretsEnum.ApiHost) ];
-const allowUserAgents = [ secretsAdapter.getSecret(SecretsEnum.PrometheusUserAgent) ];
 
 const corsOptions = (req, callback) => {
 	const origin = req.header("Origin");
-	const host = req.header("Host");
-	const userAgent = req.header("User-Agent");
 	
 	if(secretsAdapter.getSecret(SecretsEnum.Environment) === "TEST") return callback(null, true);
 
 	if (allowList.indexOf(origin) !== -1) return callback(null, true);
-
-	if (allowHosts.indexOf(host) !== -1 && allowUserAgents.indexOf(userAgent) !== -1) return callback(null, true);
 
 	callback(new Error("Not allowed cors"));
 };
